0371ab39d4f98e67850f272e67d3f11173036c16
Vasco de Krijger <vascodekrijger@gmail.com>
2015-09-11 15:02:00 +0200

    form sessions

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,24 +11,18 @@ import play.api.data.Forms._
 class Application @Inject()(val messagesApi: MessagesApi) extends Controller with I18nSupport {
 
   val SESSION_VAR = "user_value"
+  var sessionValue = ""
 
-  // Create a custom form //L   
   val sessionForm = Form(
-    single(
-      "value" -> text
+    tuple(
+      "value" -> text,
+      "session" -> text
     )
   )
 
   def index = Action { implicit request =>
-    // Check if the session variable is set
-    // If so we hand the current session with the value
-    // from SESSION_VAR so we can print this in the
-    // view
-    request.session.get(SESSION_VAR).map { item =>
-      Ok(views.html.index(sessionForm, item))
-    }.getOrElse {
-      Ok(views.html.index(sessionForm))
-    }
+    // Fill the form with the session variable
+    Ok(views.html.index(sessionForm.fill("", sessionValue), sessionValue))
   }
 
   def addToSession = Action { implicit request =>
@@ -36,18 +30,10 @@ class Application @Inject()(val messagesApi: MessagesApi) extends Controller wit
     formWithErrors => {
       BadRequest(views.html.index(formWithErrors))
     }, {
-      case (value) => {
-        var currentValue = ""
-
-        // Get the current value from the session so we
-        // can append to this later on
-        request.session.get(SESSION_VAR).map { item =>
-          currentValue = item
-        }
-
-        // Append to the current value from the session
-        Redirect(routes.Application.index()).withSession(
-            request.session + (SESSION_VAR -> (currentValue + " " + value)))
+      case (value, session) => {
+        // Update the session value iwth the new set requirement
+        sessionValue = (session + " " + value)
+        Redirect(routes.Application.index())
       }
     })
   }
--- target/scala-2.11/twirl/main/views/html/index.template.scala
+++ target/scala-2.11/twirl/main/views/html/index.template.scala
@@ -14,15 +14,15 @@ import play.api.templates.PlayMagic._
 import play.api.mvc._
 import play.api.data._
 
-class index extends BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with play.twirl.api.Template3[Form[String],String,Messages,play.twirl.api.HtmlFormat.Appendable] {
+class index extends BaseScalaTemplate[play.twirl.api.HtmlFormat.Appendable,Format[play.twirl.api.HtmlFormat.Appendable]](play.twirl.api.HtmlFormat) with play.twirl.api.Template3[Form[scala.Tuple2[String, String]],String,Messages,play.twirl.api.HtmlFormat.Appendable] {
 
   /**/
-  def apply/*1.2*/(sessionForm: Form[String], session: String = "No session data found!")(implicit messages: Messages):play.twirl.api.HtmlFormat.Appendable = {
+  def apply/*1.2*/(sessionForm: Form[(String, String)], session: String = "No session data found!")(implicit messages: Messages):play.twirl.api.HtmlFormat.Appendable = {
     _display_ {
       {
 import helper._
 
-Seq[Any](format.raw/*1.102*/("""
+Seq[Any](format.raw/*1.112*/("""
 
 """),format.raw/*4.1*/("""
 """),_display_(/*5.2*/main("Welcome to Play")/*5.25*/ {_display_(Seq[Any](format.raw/*5.27*/("""
@@ -37,18 +37,22 @@ Seq[Any](format.raw/*1.102*/("""
             field = sessionForm("value"),
             args = '_label -> "Value to add to session", 'placeholder -> "Some value"
         )),format.raw/*16.10*/("""
-        """),format.raw/*17.9*/("""<button>Add</button>
-    """)))}),format.raw/*18.6*/("""
+        """),_display_(/*17.10*/inputText(
+            field = sessionForm("session"),
+            args = 'type -> "hidden"
+        )),format.raw/*20.10*/("""
+        """),format.raw/*21.9*/("""<button>Add</button>
+    """)))}),format.raw/*22.6*/("""
 
-""")))}),format.raw/*20.2*/("""
+""")))}),format.raw/*24.2*/("""
 """))
       }
     }
   }
 
-  def render(sessionForm:Form[String],session:String,messages:Messages): play.twirl.api.HtmlFormat.Appendable = apply(sessionForm,session)(messages)
+  def render(sessionForm:Form[scala.Tuple2[String, String]],session:String,messages:Messages): play.twirl.api.HtmlFormat.Appendable = apply(sessionForm,session)(messages)
 
-  def f:((Form[String],String) => (Messages) => play.twirl.api.HtmlFormat.Appendable) = (sessionForm,session) => (messages) => apply(sessionForm,session)(messages)
+  def f:((Form[scala.Tuple2[String, String]],String) => (Messages) => play.twirl.api.HtmlFormat.Appendable) = (sessionForm,session) => (messages) => apply(sessionForm,session)(messages)
 
   def ref: this.type = this
 
@@ -61,11 +65,11 @@ Seq[Any](format.raw/*1.102*/("""
 object index extends index_Scope0.index
               /*
                   -- GENERATED --
-                  DATE: Fri Sep 11 11:50:31 CEST 2015
+                  DATE: Fri Sep 11 14:48:49 CEST 2015
                   SOURCE: /home/akatchi/Dropbox/Libraries/Playframework/activator-dist-1.3.6/opdracht-5/app/views/index.scala.html
-                  HASH: fbb20d065ebafc04fb50a277c398dc62d18734c8
-                  MATRIX: 549->1|760->101|788->120|815->122|846->145|885->147|917->153|973->184|1000->191|1033->197|1070->208|1085->214|1143->263|1183->265|1220->275|1389->423|1425->432|1481->458|1514->461
-                  LINES: 20->1|25->1|27->4|28->5|28->5|28->5|30->7|31->8|31->8|33->10|35->12|35->12|35->12|35->12|36->13|39->16|40->17|41->18|43->20
+                  HASH: c5a39146c03ebca1955d34824774a47545dfcafa
+                  MATRIX: 571->1|792->111|820->130|847->132|878->155|917->157|949->163|1005->194|1032->201|1065->207|1102->218|1117->224|1175->273|1215->275|1252->285|1421->433|1458->443|1580->544|1616->553|1672->579|1705->582
+                  LINES: 20->1|25->1|27->4|28->5|28->5|28->5|30->7|31->8|31->8|33->10|35->12|35->12|35->12|35->12|36->13|39->16|40->17|43->20|44->21|45->22|47->24
                   -- GENERATED --
               */
           
